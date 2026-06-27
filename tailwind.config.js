96437aa5b114cde89b51e0e39d80dbcab0edc01e
foad2kk <foad2kk@gmail.com>
2026-06-27 10:20:53 +0300

    Add remaining config files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Cairo', 'sans-serif'],
+        arabic: ['Cairo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
